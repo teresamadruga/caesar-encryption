04530f5b08db719f629659134cc70f3bf7175cb1
teresamadruga <madrugat@gmail.com>
2023-01-14 05:35:06 +0000

    added different parsers per type of encryption

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from cypher import caesar_cypher, vigenere_cypher, encrypt
 
 
 args = args_parse()
-# print(encrypt(caesar_cypher, args.input, args.shift, args.key))
-print(encrypt(vigenere_cypher, args.input, 'lemon', args.key))
+if args.command == 'caesar':
+    print(encrypt(caesar_cypher, args.input, args.shift, args.key))
+elif args.command == 'vigenere':
+    print(encrypt(vigenere_cypher, args.input, args.shift, args.key))
--- parse.py
+++ parse.py
@@ -9,11 +9,21 @@ ALPHABETS = {
 
 def args_parse():
     parser = argparse.ArgumentParser(description = 'encrypt text using caesar cypher', prog = "caesar's code", usage = '%(prog)s [options]')
+    
     parser.add_argument('-t','--text', type=str, help='input text to encrypt', default='')
-    parser.add_argument('-s','--shift', type = int, required = True, help='number of positions to shift the key')
     parser.add_argument('-a','--alphabet', type = str, required = True, help='alphabet used as key', default='en')
+
+    cypher_type = parser.add_subparsers(dest='command')
+    caesar = cypher_type.add_parser('caesar')
+    caesar.add_argument('-s','--shift', type = int, required = True, help='number of positions to shift the key')   
+    vigenere = cypher_type.add_parser('vigenere')
+    vigenere.add_argument('-vt','--vigenere_table', type = str, required = True, help='table that indicates the number of positions to shift the key')
+    
     args = parser.parse_args()
     input = args.text.lower()
     args.input = input
+    if args.command == 'vigenere':
+        args.shift = args.vigenere_table
     args.key = ALPHABETS[args.alphabet]
+
     return args
