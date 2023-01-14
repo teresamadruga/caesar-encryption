b9905a86a533d3094e69d2857070ab18847c2072
teresamadruga <madrugat@gmail.com>
2023-01-14 05:38:21 +0000

    refactored vigenere_cypher

--- cypher.py
+++ cypher.py
@@ -8,10 +8,9 @@ def caesar_cypher(char_idx, char, shift, key):
 def vigenere_cypher(char_idx, char, table, key):
     ''' https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher
     '''
-    table_item = table[char_idx % len(table)]
-    shift = key.index(table_item)
+    shift = key.index(table[char_idx % len(table)])
     return key[((key.index(char) + shift) % len(key))]
-    
+
 
 def encrypt(*args):
     '''
