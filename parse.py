bc5d435bb86e9b3286959705d4feb5a1461b75e9
teresamadruga <madrugat@gmail.com>
2023-01-14 05:50:43 +0000

    updated parse.py

--- parse.py
+++ parse.py
@@ -11,6 +11,7 @@ def args_parse():
     parser = argparse.ArgumentParser(description = 'encrypt text using caesar cypher', prog = "caesar's code", usage = '%(prog)s [options]')
     
     parser.add_argument('-t','--text', type=str, help='input text to encrypt', default='')
+    parser.add_argument('-f','--filename', type=str, help='path of file with input text to encrypt', default='')
     parser.add_argument('-a','--alphabet', type = str, required = True, help='alphabet used as key', default='en')
 
     cypher_type = parser.add_subparsers(dest='command')
@@ -20,7 +21,12 @@ def args_parse():
     vigenere.add_argument('-vt','--vigenere_table', type = str, required = True, help='table that indicates the number of positions to shift the key')
     
     args = parser.parse_args()
-    input = args.text.lower()
+    input = ''
+    if args.text != '':
+        input += args.text.lower()
+    if args.filename != None:
+        # TODO: implement read file
+        input += ' ' + args.filename
     args.input = input
     if args.command == 'vigenere':
         args.shift = args.vigenere_table
