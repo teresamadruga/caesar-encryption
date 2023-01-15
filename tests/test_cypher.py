b1be96036bf6502b88503caf034d43d24ae26f20
teresamadruga <madrugat@gmail.com>
2023-01-15 07:48:01 +0000

    added tests with non alphabets. vigenere doesn't pass

--- tests/test_cypher.py
+++ tests/test_cypher.py
@@ -6,7 +6,7 @@ from src.cypher import caesar_cypher, encrypt, vigenere_cypher
 from src.parse import ALPHABETS
 
 
-def test_on_caesar():
+def test_on_caesar_alphabets():
     """Test caesar code result"""
     assert (
         encrypt(caesar_cypher, "ezevkvveky tveklip", 9, ALPHABETS["en"])
@@ -14,9 +14,25 @@ def test_on_caesar():
     )
 
 
-def test_on_vigenere():
+def test_on_caesar_non_alphabets():
+    """Test caesar code result"""
+    assert (
+        encrypt(caesar_cypher, "&*48(£^ )+ =_39-", 15, ALPHABETS["es"])
+        == "&*48(£^ )+ =_39-"
+    )
+
+
+def test_on_vigenere_alphabets():
     """Test vigenere code result"""
     assert (
         encrypt(vigenere_cypher, "attackatdawn", "lemon", ALPHABETS["en"])
         == "lxfopvefrnhr"
+    )
+
+
+def test_on_vigenere_non_alphabets():
+    """Test vigenere code result"""
+    assert (
+        encrypt(vigenere_cypher, "attackatdawn", "l3m0n", ALPHABETS["en"])
+        == "ltfapvafdnhn"
     )
